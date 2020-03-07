d9a244cf12e890c88d4250b11e82834bb887c795
Janbolotnr <janbolotnr@gmail.com>
2020-03-08 00:11:15 +0600

    ch3part1task3

---
+++ part3task3.py
@@ -0,0 +1,44 @@
+user = float(input("Enter size: "))
+if user == 3.3:
+    print("Your size: ", user)
+    a = ("---- ---- ----")
+    b = ("|  | |  | |  |")
+    c = ("---- ---- ----")
+    d = ("|  | |  | |  |")
+    e = ("---- ---- ----")
+    f = ("|  | |  | |  |")
+    g = ("---- ---- ----")
+
+    print("Your table: ")
+
+    print(a)
+    print(b)
+    print(c)
+    print(d)
+    print(e)
+    print(f)
+    print(g)
+else:
+    print("Wrong type!!!")
+  
+
+# a = ("---- ---- ----")
+# b = ("|  | |  | |  |")
+# c = ("---- ---- ----")
+# d = ("|  | |  | |  |")
+# e = ("---- ---- ----")
+# f = ("|  | |  | |  |")
+# g = ("---- ---- ----")
+
+# print("Your table: ")
+
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(e)
+# print(f)
+# print(g)
+
+# a = ("___  ___  ___")
+# print(a)
